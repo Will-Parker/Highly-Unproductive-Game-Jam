d4c1fae402ac7d8a55a5d1b97e5961bdebf627c2
sonicpokemonguy <WilliamParkerProfessionalEmail@gmail.com>
2023-05-10 20:01:03 -0400

    add grid based movement

--- Assets/Scripts/CharacterControl.cs
+++ Assets/Scripts/CharacterControl.cs
@@ -5,17 +5,19 @@ using UnityEngine.InputSystem;
 
 public class CharacterControl : MonoBehaviour
 {
-    // Reference to Rigidbody component
-    // Temporary probably
-    private Rigidbody2D rb;
     // Unity New Input System Controls
-    public Controls controls;
+    private Controls controls;
+    // point player moves toward
+    [SerializeField] private Transform movePoint;
+    // impassable layer
+    [SerializeField] private LayerMask impassableLayer;
+
+    [SerializeField] [Range(0, 20)] private float moveSpeed;
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
-        if (rb == null)
-            Debug.Log("Rigidbody2D could not be found");
+        if (movePoint == null)
+            Debug.Log("Move Point not assigned");
 
         // Enable Gameplay controls
         controls = new Controls();
@@ -23,13 +25,54 @@ public class CharacterControl : MonoBehaviour
         controls.Gameplay.Move.performed += Move_performed;
     }
 
+    private void Start()
+    {
+        movePoint.parent = null;
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+    }
+
     private void Move_performed(InputAction.CallbackContext context)
     {
         Vector2 inputVector = context.ReadValue<Vector2>();
-        // TODO: Movement along grid
+        if (Vector3.Distance(transform.position, movePoint.position) <= 0.05f)
+        {
+            transform.position = movePoint.position;
+            if (Mathf.Abs(inputVector.x) == 1f)
+            {
+                Vector2 moveDir = new (inputVector.x, 0f);
+                RaycastHit2D ray = Physics2D.Raycast(Vec3ToVec2(transform.position), moveDir, 1f, impassableLayer);
+                if (!ray)
+                {
+                    movePoint.position += Vec2ToVec3(moveDir);
+                }
+            }
+            else if (Mathf.Abs(inputVector.y) == 1f)
+            {
+                Vector2 moveDir = new (0f, inputVector.y);
+                RaycastHit2D ray = Physics2D.Raycast(Vec3ToVec2(transform.position), moveDir, 1f, impassableLayer);
+                if (!ray)
+                {
+                    movePoint.position += Vec2ToVec3(moveDir);
+                }
+            }
+        }
         // TODO: animate movement
         // TODO: move all allies
         // TODO: move camera (actually probably not handled here)
-        rb.AddForce(new Vector2(inputVector.x, inputVector.y) * 2f, ForceMode2D.Impulse);
+    }
+
+
+    private Vector2 Vec3ToVec2(Vector3 v)
+    {
+        return new Vector2(v.x, v.y);
+    }
+
+    private Vector3 Vec2ToVec3(Vector2 v)
+    {
+        return new Vector3(v.x, v.y, 0f);
     }
 }

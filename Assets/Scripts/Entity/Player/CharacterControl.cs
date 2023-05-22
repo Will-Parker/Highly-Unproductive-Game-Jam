6eff2508f9a400cc40188bb653999ef9d429c02d
William Parker <89484741+Will-Parker@users.noreply.github.com>
2023-05-21 23:20:47 -0400

    refactor character control into party manager (#19)

--- Assets/Scripts/Entity/Player/CharacterControl.cs
+++ Assets/Scripts/Entity/Player/CharacterControl.cs
@@ -10,16 +10,6 @@ public class CharacterControl : MonoBehaviour
     private PartyManager pm;
     // Unity New Input System Controls
     private Controls controls;
-    // points each ally moves toward
-    private Vector3[] movePoints = new Vector3[4]; // hardcoded to 4
-    // impassable layer
-    [SerializeField] private LayerMask impassableLayer;
-    // how quickly it moves to next tile
-    [SerializeField] [Range(0, 20)] private float moveSpeed;
-
-    private MoveState moveState = MoveState.NotMoving;
-
-    private Vector3 prevTail;
 
     private void Awake()
     {
@@ -34,85 +24,21 @@ public class CharacterControl : MonoBehaviour
         controls.Gameplay.SpecialAction.performed += SpecialAction_performed;
     }
 
-    private void Start()
-    {
-        for (int i = 0; i < 4; i++)
-        {
-            movePoints[i] = pm.allies[i].transform.position;
-            //pm.allies[i].GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1);
-        }
-
-        prevTail = new Vector3(pm.allies[3].transform.position.x - 1, pm.allies[3].transform.position.y, pm.allies[3].transform.position.z);
-
-        //Time.timeScale = 0.1f;
-    }
-
-    private void Update()
-    {
-        if (moveState != MoveState.NotMoving)
-        {
-            for (int i = 0; i < 4; i++)
-                pm.allies[i].transform.position = Vector3.MoveTowards(pm.allies[i].transform.position, movePoints[i], moveSpeed * Time.deltaTime);
-            
-            if (moveState == MoveState.Moving)
-            {
-                if (Vector3.Distance(pm.allies[0].transform.position, movePoints[0]) <= 0.05f)
-                {
-                    for (int i = 0; i < 4; i++)
-                        pm.allies[i].transform.position = movePoints[i];
-                    RotateParty();
-                }
-            }
-            else if (moveState == MoveState.Rotating)
-            {
-                if (Vector3.Distance(pm.allies[0].transform.position, movePoints[0]) <= 0.05f)
-                {
-                    for (int i = 0; i < 4; i++)
-                    {
-                        pm.allies[i].transform.position = movePoints[i];
-                        //pm.allies[i].UpdateAnim(false);
-                    }
-                    moveState = MoveState.NotMoving;
-                    FindObjectOfType<GameStateManager>().EndTurn();
-                }
-            }
-        }
-    }
-
     private void Move_performed(InputAction.CallbackContext context)
     {
         GameStateManager gsm = FindObjectOfType<GameStateManager>();
         if (gsm.GetGameState() == GameState.Player)
         {
             Vector2 inputVector = context.ReadValue<Vector2>();
-            if (moveState == MoveState.NotMoving)
+            if (Mathf.Abs(inputVector.x) == 1f)
             {
-                if (Mathf.Abs(inputVector.x) == 1f)
-                {
-                    Vector2 moveDir = new(inputVector.x, 0f);
-                    RaycastHit2D ray = Physics2D.Raycast(Vec3ToVec2(pm.allies[0].transform.position), moveDir, 1f, impassableLayer);
-                    if (!ray
-                        && Vector2.Distance(Vec3ToVec2(pm.allies[0].transform.position) + moveDir, Vec3ToVec2(pm.allies[1].transform.position)) > 0.05f
-                        && Vector2.Distance(Vec3ToVec2(pm.allies[0].transform.position) + moveDir, Vec3ToVec2(pm.allies[2].transform.position)) > 0.05f
-                        && !WillLandOnEnemy(moveDir))
-                    {
-                        MoveParty(moveDir);
-                        StartCoroutine(SpriteFadeOutFadeIn(pm.allies[0].GetComponent<SpriteRenderer>(), 2f / moveSpeed));
-                    }
-                }
-                else if (Mathf.Abs(inputVector.y) == 1f)
-                {
-                    Vector2 moveDir = new(0f, inputVector.y);
-                    RaycastHit2D ray = Physics2D.Raycast(Vec3ToVec2(pm.allies[0].transform.position), moveDir, 1f, impassableLayer);
-                    if (!ray
-                        && Vector2.Distance(Vec3ToVec2(pm.allies[0].transform.position) + moveDir, Vec3ToVec2(pm.allies[1].transform.position)) > 0.05f
-                        && Vector2.Distance(Vec3ToVec2(pm.allies[0].transform.position) + moveDir, Vec3ToVec2(pm.allies[2].transform.position)) > 0.05f
-                        && !WillLandOnEnemy(moveDir))
-                    {
-                        MoveParty(moveDir);
-                        StartCoroutine(SpriteFadeOutFadeIn(pm.allies[0].GetComponent<SpriteRenderer>(), 2f / moveSpeed));
-                    }
-                }
+                Vector2 moveDir = new(inputVector.x, 0f);
+                pm.AttemptMove(moveDir);
+            }
+            else if (Mathf.Abs(inputVector.y) == 1f)
+            {
+                Vector2 moveDir = new(0f, inputVector.y);
+                pm.AttemptMove(moveDir);
             }
         }
     }
@@ -120,101 +46,9 @@ public class CharacterControl : MonoBehaviour
     private void SpecialAction_performed(InputAction.CallbackContext context)
     {
         GameStateManager gsm = FindObjectOfType<GameStateManager>();
-        if (gsm.GetGameState() == GameState.Player && moveState == MoveState.NotMoving)
+        if (gsm.GetGameState() == GameState.Player)
         {
-            moveState = MoveState.SpecialAction;
-            pm.allies[0].PeformSpecialAction();
-            StartCoroutine(SpriteFadeOutFadeIn(pm.allies[0].GetComponent<SpriteRenderer>(), 2f / moveSpeed));
-            StartCoroutine(WaitToRotate(1f / moveSpeed));
+            pm.AttemptRotate();
         }
-    }
-
-    private void MoveParty(Vector2 moveDir)
-    {
-        moveState = MoveState.Moving;
-        movePoints[0] += Vec2ToVec3(moveDir);
-        pm.allies[0].facingDirection = moveDir;
-        //pm.allies[0].UpdateAnim(true, pm.allies[0].facingDirection);
-        prevTail = pm.allies[3].transform.position;
-        for (int i = 1; i < 4; i++)
-        {
-            movePoints[i] = pm.allies[i - 1].transform.position;
-            pm.allies[i].facingDirection = Vec3ToVec2(movePoints[i] - pm.allies[i].transform.position);
-            //pm.allies[i].UpdateAnim(true, pm.allies[i].facingDirection);
-        }
-    }
-
-    private void RotateParty()
-    {
-        moveState = MoveState.Rotating;
-
-        pm.allies[0].transform.position = prevTail;
-
-        pm.RotatePartyPositions();
-
-        prevTail = pm.allies[3].transform.position;
-        pm.allies[0].facingDirection = Vec3ToVec2(movePoints[0] - pm.allies[0].transform.position);
-        //pm.allies[0].UpdateAnim(true, pm.allies[0].facingDirection));
-        for (int i = 1; i < 4; i++)
-        {
-            movePoints[i] = pm.allies[i - 1].transform.position;
-            pm.allies[i].facingDirection = Vec3ToVec2(movePoints[i] - pm.allies[i].transform.position);
-            //pm.allies[i].UpdateAnim(true, pm.allies[i].facingDirection));
-        }
-    }
-
-    private enum MoveState
-    {
-        NotMoving,
-        Moving,
-        Rotating,
-        SpecialAction,
-        Dead
-    }
-
-    // Credit: https://answers.unity.com/questions/1687634/how-do-i-mathflerp-the-spriterendereralpha.html
-    private IEnumerator SpriteFadeOutFadeIn(SpriteRenderer sr, float duration)
-    {
-        float elapsedTime = 0;
-        while (elapsedTime < duration / 2)
-        {
-            elapsedTime += Time.deltaTime;
-            float newAlpha = Mathf.Lerp(1f, 0f, elapsedTime / (duration / 2));
-            sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, newAlpha);
-            yield return null;
-        }
-        sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, 0f);
-        while (elapsedTime < duration)
-        {
-            elapsedTime += Time.deltaTime;
-            float newAlpha = Mathf.Lerp(0f, 1f, (elapsedTime - (duration / 2)) / (duration / 2));
-            sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, newAlpha);
-            yield return null;
-        }
-        sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, 1f);
-    }
-
-    private IEnumerator WaitToRotate(float duration)
-    {
-        yield return new WaitForSeconds(duration);
-        RotateParty();
-    }
-
-    private bool WillLandOnEnemy(Vector2 moveDir)
-    {
-        Enemy[] enemies = FindObjectsOfType<Enemy>();
-        foreach (Enemy enemy in enemies)
-        {
-            if (Vector2.Distance(Vec3ToVec2(pm.allies[0].transform.position) + moveDir, Vec3ToVec2(enemy.transform.position)) <= 0.05f)
-                return true;
-        }
-        return false;
-    }
-
-    public void PassTurn()
-    {
-        moveState = MoveState.Dead;
-        StartCoroutine(SpriteFadeOutFadeIn(pm.allies[0].GetComponent<SpriteRenderer>(), 2f / moveSpeed));
-        StartCoroutine(WaitToRotate(1f / moveSpeed));
     }
 }
